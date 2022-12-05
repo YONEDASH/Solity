175df1b45ac5109d3808aa07aea58a38667f3911
YONEDASH <til211@gmx.de>
2022-12-05 17:42:53 +0100

    Revert lighting changes

--- src/main/java/de/yonedash/solity/graphics/EntityLighting.java
+++ src/main/java/de/yonedash/solity/graphics/EntityLighting.java
@@ -24,51 +24,50 @@ public class EntityLighting extends EntityBase implements VisualEffect {
 
     @Override
     public void draw(Scene scene, Graphics2D g2d, double dt) {
-        g2d.drawImage(
-                image,
-                scene.scaleToDisplay(this.boundingBox.position.x - 1),
-                scene.scaleToDisplay(this.boundingBox.position.y - 1),
-                scene.scaleToDisplay(this.boundingBox.size.x),
-                scene.scaleToDisplay(this.boundingBox.size.y),
-                null
-        );
+//        g2d.drawImage(
+//                image,
+//                scene.scaleToDisplay(this.boundingBox.position.x - 1),
+//                scene.scaleToDisplay(this.boundingBox.position.y - 1),
+//                scene.scaleToDisplay(this.boundingBox.size.x),
+//                scene.scaleToDisplay(this.boundingBox.size.y),
+//                null
+//        );
     }
 
     public void updateLighting(Scene scene, ArrayList<DisplayEntity> litUpEntities) {
-        if (this.image == null)
-            return;
-
-        Graphics2D g2d = (Graphics2D) this.image.getGraphics();
-        if (g2d == null) g2d = this.image.createGraphics();
-
-        g2d.clearRect(0, 0, image.getWidth(), image.getHeight());
-
-        g2d.setColor(new Color(0, 0, 0, 90));
-        g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
-
-        double baseSize = Tile.TILE_SIZE;
-
-        double lightingHorizontalScale = this.image.getWidth() / this.boundingBox.size.x;
-        double lightingVerticalScale = this.image.getHeight() / this.boundingBox.size.y;
-        double scale = ((lightingVerticalScale + lightingHorizontalScale) / 2.0);
-
-        for (DisplayEntity displayEntity : litUpEntities) {
-            Vec2D lightPos = displayEntity.getBoundingBox().center().clone().subtract(this.boundingBox.position).multiply(scale);
-            LightSource source = displayEntity.getLightSource();
-            if (source == null) continue;
-
-            Color color = source.color();
-            float red = color.getRed() / 255.0f;
-            float green = color.getGreen() / 255.0f;
-            float blue = color.getBlue() / 255.0f;
-            float alpha = 1.0f - ((red + green + blue) / 3.0f);
-
-            double radius = baseSize * source.brightness() / 2.0;
-
-            g2d.setColor(new Color(red, green, blue, 1.0f));
-            g2d.fillOval((int) (lightPos.x - radius), (int) (lightPos.y - radius), (int) radius, (int) radius);
-        }
-
+//        if (this.image == null)
+//            return;
+//
+//        Graphics2D g2d = (Graphics2D) this.image.getGraphics();
+//        if (g2d == null) g2d = this.image.createGraphics();
+//
+//        g2d.clearRect(0, 0, image.getWidth(), image.getHeight());
+//
+//        g2d.setColor(new Color(0, 0, 0, 90));
+//        g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
+//
+//        double baseSize = Tile.TILE_SIZE;
+//
+//        double lightingHorizontalScale = this.image.getWidth() / this.boundingBox.size.x;
+//        double lightingVerticalScale = this.image.getHeight() / this.boundingBox.size.y;
+//        double scale = ((lightingVerticalScale + lightingHorizontalScale) / 2.0);
+//
+//        for (DisplayEntity displayEntity : litUpEntities) {
+//            Vec2D lightPos = displayEntity.getBoundingBox().center().clone().subtract(this.boundingBox.position).multiply(scale);
+//            LightSource source = displayEntity.getLightSource();
+//            if (source == null) continue;
+//
+//            Color color = source.color();
+//            float red = color.getRed() / 255.0f;
+//            float green = color.getGreen() / 255.0f;
+//            float blue = color.getBlue() / 255.0f;
+//            float alpha = 1.0f - ((red + green + blue) / 3.0f);
+//
+//            double radius = baseSize * source.brightness() / 2.0;
+//
+//            g2d.setColor(new Color(red, green, blue, 1.0f));
+//            g2d.fillOval((int) (lightPos.x - radius), (int) (lightPos.y - radius), (int) radius, (int) radius);
+//        }
     }
 
     @Override
